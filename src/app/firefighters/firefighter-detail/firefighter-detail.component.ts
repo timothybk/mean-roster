ab2c04bfcfb0f960a7fb0ff6e82197e546143530
Tim Buckland-Kerr <timbk86@gmail.com>
2017-09-02 05:39:44 +1000

    unsubscribe from firefighter service

--- src/app/firefighters/firefighter-detail/firefighter-detail.component.ts
+++ src/app/firefighters/firefighter-detail/firefighter-detail.component.ts
@@ -1,16 +1,18 @@
 import { Firefighter } from './../firefighter.model';
 import { ActivatedRoute, Router, Params } from '@angular/router';
 import { FirefightersService } from './../firefighters.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs/subscription';
 
 @Component({
   selector: 'app-firefighter-detail',
   templateUrl: './firefighter-detail.component.html',
   styleUrls: ['./firefighter-detail.component.css']
 })
-export class FirefighterDetailComponent implements OnInit {
+export class FirefighterDetailComponent implements OnInit, OnDestroy {
   firefighter: Firefighter;
   index: number;
+  subscription: Subscription;
 
   constructor(private fireFightersService: FirefightersService,
     private route: ActivatedRoute,
@@ -21,7 +23,7 @@ export class FirefighterDetailComponent implements OnInit {
     .subscribe(
       (params: Params) => {
         this.index = +params['id'];
-        this.fireFightersService.firefighters
+        this.subscription = this.fireFightersService.firefighters
           .subscribe(
             (firefighters: Firefighter[]) => {
               this.firefighter = firefighters[this.index];
@@ -29,6 +31,10 @@ export class FirefighterDetailComponent implements OnInit {
         console.log(this.firefighter);
       }
     );
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
--- src/app/firefighters/firefighter-list/firefighter-list.component.ts
+++ src/app/firefighters/firefighter-list/firefighter-list.component.ts
@@ -1,6 +1,7 @@
+import { Subscription } from 'rxjs/subscription';
 import { Firefighter } from './../firefighter.model';
 import { FirefightersService } from './../firefighters.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -8,16 +9,17 @@ import { Router, ActivatedRoute } from '@angular/router';
   templateUrl: './firefighter-list.component.html',
   styleUrls: ['./firefighter-list.component.css']
 })
-export class FirefighterListComponent implements OnInit {
+export class FirefighterListComponent implements OnInit, OnDestroy {
 
   firefighters: Firefighter[];
+  subscription: Subscription;
 
   constructor(private firefightersService: FirefightersService,
     private router: Router,
     private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.firefightersService.firefighters
+    this.subscription = this.firefightersService.firefighters
       .subscribe(
       firefighters => {
         this.firefighters = firefighters;
@@ -30,6 +32,10 @@ export class FirefighterListComponent implements OnInit {
 
   onNewFireFighter() {
     this.router.navigate(['new'], {relativeTo: this.route});
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
